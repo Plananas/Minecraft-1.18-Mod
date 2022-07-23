cafb819e7b182618227c7466f5f0afd6505a2889
Plananas <robinharris2003>
2022-07-23 23:18:34 +0100

    Created a Nuke Item in Minecraft. Item has 0 functionality so far.

---
+++ src/main/java/com/example/examplemod/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.example.examplemod.item;
+
+import com.example.examplemod.FirstMod;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FirstMod.MOD_ID);
+
+    public static final RegistryObject<Item> NUKE = ITEMS.register( "nuke",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
